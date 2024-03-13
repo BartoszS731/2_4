8c0f6aad1b92fc9e7d75673ddf1f847324bb6d95
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-03-13 14:31:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner scanner=new Scanner(System.in);
+    System.out.println("Wybierz operacje:");
+    System.out.println("1: dodawanie");
+    System.out.println("2: odejmowanie");
+    System.out.println("3: mnożenie");
+    System.out.println("4: dzielenie");
+    int wybor=scanner.nextInt();
+  System.out.println("Podaj pierwszą liczbe:");
+    double liczba1=scanner.nextDouble();
+    System.out.println("Podaj drugą liczbe:");
+      double liczba2=scanner.nextDouble();
+    double wynik=0
+    switch(wybor)
+      {
+        case 1:
+          
+
+
+
+
+
+
+          
+      }
+      
+
+
+
+
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
